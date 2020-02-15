cbe73e3ccad08abc0fd14579e8a0f04ced3cfc9a
Anurag Shukla <shukla.anurag0006@gmail.com>
2020-02-15 17:03:15 +0530

    admin static issue fixed.

--- Translation_backend/settings.py
+++ Translation_backend/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-# Activate Django-Heroku.
+STATIC_ROOT=os.path.join(BASE_DIR,'static/')
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR,'static/'),
 )
